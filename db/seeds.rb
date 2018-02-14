a6b49076311d62ceee45e1775b70d0308f8f8235
Victor Sassonia <victor.sassonia@gmail.com>
2018-02-14 17:41:35 +0100

    start simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 10 fake flats...'
+10.times do
+  Flat.create!(
+    name: Faker::Name.unique.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence(1),
+    price_per_night: Faker::Number.between(30, 800),
+    number_of_guests: Faker::Number.between(1, 8),
+    picture: "https://picsum.photos/200/300/?random"
+    )
+end
+puts 'Finished!'
